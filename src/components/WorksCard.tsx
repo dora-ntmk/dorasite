b272d23453e0bdbc2fe79c15c10501f88fac68cc
どら <dora@natamoko.jp>
2026-05-24 23:55:40 +0900

    バグ修正しました CSRはなんか環境の都合でできなかったので更新したら再デプロイする感じでやる（Reactから戻すの面倒くさいのでそのまま）

--- src/components/WorksCard.tsx
+++ src/components/WorksCard.tsx
@@ -10,7 +10,7 @@ export default function WorksCard({ imgSrc, title, date, url }: Props) {
     <a href={url} target="_blank" rel="noopener noreferrer">
       <div className="relative rounded-md aspect-square">
         <img src={imgSrc} className="w-full h-full object-cover rounded-md" />
-        <div className="absolute inset-0 bg-black/50 rounded-md flex flex-col items-center justify-center duration-200 opacity-0 [@media(hover:hover)_and_(pointer:fine)]:hover:opacity-100 [@media(hover:hover)_and_(pointer:fine)]:hover:backdrop-blur-xs">
+        <div className="absolute inset-0 bg-black/50 rounded-md p-3 flex flex-col items-center justify-center text-center duration-200 opacity-0 [@media(hover:hover)_and_(pointer:fine)]:hover:opacity-100 [@media(hover:hover)_and_(pointer:fine)]:hover:backdrop-blur-xs">
           <span className="text-white font-bold text-lg">{title}</span>
           <span className="text-gray-300 text-sm">{date}</span>
         </div>
