f23045095d9420fcdda74b840d4947069c81f1a8
ど　ら <199174994+dora-ntmk@users.noreply.github.com>
2026-05-24 23:48:38 +0900

    Revert "Add Cloudflare Workers configuration"

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,10 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 
-import cloudflare from '@astrojs/cloudflare';
-
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-
   integrations: [react()],
-  adapter: cloudflare(),
 });
