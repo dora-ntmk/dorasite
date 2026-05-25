547a8972f3e72bb1437074947a38e327cef2634a
どら <dora@natamoko.jp>
2026-05-25 10:33:06 +0900

    例外処理追加

--- src/pages/api/works.ts
+++ src/pages/api/works.ts
@@ -4,7 +4,7 @@ import { createClient } from 'microcms-js-sdk';
 export const GET: APIRoute = async () => {
   const client = createClient({
     serviceDomain: 'dorashark',
-    apiKey: import.meta.env.SECRET_SHARK_API_KEY,
+    apiKey: import.meta.env.SECRET_SHARK_API_KEY ?? "",
   });
 
   const items = await client.getAllContents({ endpoint: 'works' }).catch(() => []);
